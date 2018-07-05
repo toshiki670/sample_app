d04af5250eef51cc8e36aaa8d971fc55993045e8
Toshiki <toshiki.k.000@gmail.com>
2018-07-05 16:27:25 +0900

    Add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
+  # static_pages_home_url getを記述することでヘルパーが生成される
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
