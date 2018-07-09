38889aaeab337488eabde431bf18d1af2b8c9dae
Toshiki <toshiki.k.000@gmail.com>
2018-07-09 09:55:29 +0900

    Add User model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@Example.com")
+  end
+
+  test "Should be valid" do
+    assert @user.valid?
+  end
+
+  test "Name should be present" do
+    @user.name = '    '
+    assert_not @user.valid?
+  end
 end
