61d870ff2418d84afb2e2dc25cec2015089a0b36
Toshiki <toshiki.k.000@gmail.com>
2018-07-09 16:20:54 +0900

    Case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\Z/
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+            uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -53,6 +53,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "Email addresses should be unique" do
     duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
