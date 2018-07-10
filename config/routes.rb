b562f4b5bdb6ce017b1f59c76c4589e140c1592d
Toshiki <toshiki.k.000@gmail.com>
2018-07-10 18:11:23 +0900

    Add show action

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'user#new'
 
-  resources :users
+  resources :user
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
