0171a74e34f903dd436400d0a98d5b10c6b01c7b
Toshiki <toshiki.k.000@gmail.com>
2018-07-10 15:00:56 +0900

    Add CRUD users

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'user#new'
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
