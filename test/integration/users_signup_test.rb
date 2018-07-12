9af7aa773e91e3425cecaeaa6b65003193d18911
Toshiki <toshiki.k.000@gmail.com>
2018-07-12 10:23:47 +0900

    Change integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,13 +3,14 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
-    assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name: "",
-                                         email: "user@invalid",
-                                         password: "foo",
-                                         password_confirmation: "bar" } }
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
     end
-    assert_template 'users/new'
+    follow_redirect!
+    assert_template 'users/show'
     # assert_select 'div#<CSS id for error explanation>'
     # assert_select 'div.<CSS class for field with error>'
   end
